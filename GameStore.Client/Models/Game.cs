0db0bb6da65cea4eeec4fd12275aafc4b9547629
avardag <steklofan@gmail.com>
2023-09-17 17:45:08 +0200

    add valdation on input fields

--- GameStore.Client/Models/Game.cs
+++ GameStore.Client/Models/Game.cs
@@ -1,10 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace GameStore.Client.Models;
 
 public class Game
 {
     public int Id { get; set; }
+    [Required]
+    [StringLength(64)]
     public required string Name { get; set; } = string.Empty;
+    [Required]
+    [StringLength(32)]
     public required string Genre { get; set; }
+    [Required]
+    [Range(1, 200)]
     public decimal Price { get; set; }
     public DateTime ReleaseDate { get; set; }
 }
